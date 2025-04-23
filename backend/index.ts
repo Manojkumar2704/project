e06dc9b9c592c084a6cc9af02e761ca5c29195ab
manoj kumar <manojkumarp.ait@outlook.com>
2025-04-23 19:14:12 +0530

    first commit

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,14 @@
 import express, { Request, Response } from 'express';
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+const  User =require('./db/db') ; 
+const bcrypt=require("bcrypt")
+const jwt=require("jsonwebtoken")
+
 const app = express();
 const port = 7000;
+const secretKey="manoj"
+
+app.use(express.json());
 
 function connectToDatabase() {
     mongoose.connect("mongodb://localhost:27017/taskdb");
@@ -18,6 +25,57 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello from the backend!');
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port http://localhost:${port}`);
+app.post("/register",async (req: Request, res: Response) => {
+  try {
+    const { userName, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+       res.status(400).json({ message: "Email already in use" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      userName,
+      email,
+      password: hashedPassword,
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json({
+      user: {
+        userName: savedUser.userName,
+        email: savedUser.email,
+      },
+      message: "User registered successfully",
+    });
+
+  } catch (error) {
+    console.error("Registration Error:", error);
+    res.status(500).json({ message: "Error registering user", error });
+  }
 });
+
+
+app.post("/login", async (req: Request, res: Response) => {
+  const { userName, password } = req.body;
+
+  const user =await User.findOne({userName});
+  if (user) {
+    const ispasswordValid = await bcrypt.compare(password, user.password);
+    if (ispasswordValid) {
+      const token = await jwt.sign({ userName }, secretKey, {
+        expiresIn: "1h",
+      });
+      res.json({ token });
+    } else {
+      console.log("invalid password for the user", userName);
+      res.status(400).json({ message: "Invalid credentials" });
+    }
+  } else {
+    console.log("user not found : ", userName);
+    res.status(401).json({ message: "Invalid credentials" });
+  }
+});
+
+
+
+app.listen(port,()=>console.log("connected to port http://localhost:"+port)
+)
